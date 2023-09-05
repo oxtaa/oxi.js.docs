e9b7e594b099ff3f3ae4d2e19cdedac6e68c5067
oxta! <75595163+oxtaa@users.noreply.github.com>
2023-09-05 16:15:17 -0300

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
-
+import fetch from 'node-fetch';
+let text = 'a'
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Latest version',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        ${text}
       </>
     ),
   },
