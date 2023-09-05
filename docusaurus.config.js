585acb62ac16065f1f0273d1a7ad389d8772cf27
oxta! <75595163+oxtaa@users.noreply.github.com>
2023-09-05 17:55:00 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'oxi.js',
+  title: 'OxiProjects',
   tagline: 'An aoi.js v5.5.5 fork that adds extra functions and updates important stuff',
   url: 'https://oxi.js.org',
   baseUrl: '/',
