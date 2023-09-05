eabb5028553e6a70634cf251ea0246fafd062b1e
oxta! <75595163+oxtaa@users.noreply.github.com>
2023-09-05 17:37:25 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,12 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
-const name = 'oxi.js';
-
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{name}</h1>
+        <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
@@ -32,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title="OxiProjects"
+      title={`${siteConfig.title}`}
       description="oxi.js - An aoi.js v5.5.5 fork that adds extra functions and updates important stuff">
       <HomepageHeader />
       <main>
