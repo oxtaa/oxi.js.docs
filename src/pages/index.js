f78b5d8d12bd37fe1ff79424939da7285e840e9b
oxta! <75595163+oxtaa@users.noreply.github.com>
2023-09-07 12:44:07 -0300

    Actualizar index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link className="button button--secondary button--lg" href="https://npmjs.com/package/oxi.js">
             NPM page
           </Link>
-          <Link style={{marginsLeft:'4px',marginsRight:'4px'}} className="button button--secondary button--lg" to="/docs/intro">
+          <Link style={{marginsRight:'10px'}} className="button button--secondary button--lg" to="/docs/intro">
             Docs
           </Link>
         </div>
